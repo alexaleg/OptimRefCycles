f9eb4b9d44cf200083093103b1b98d467bd2cdb8
alexaleg <aleguizamonr@gmail.com>
2017-07-13 16:40:08 -0500

    Removed NotNeeded Folder

--- NotNeeded/propTest.py
+++
@@ -1,9 +0,0 @@
-import CoolProp as CP
-
-HEOS = CP.AbstractState("HEOS", "Methane&Ethane")
-
-HEOS.set_mole_fractions([0.2, 0.8])
-HEOS.update(CP.PT_INPUTS, 101325, 200)
-HEOS.mole_fractions_liquid()
-HEOS.mole_fractions_vapor()
-HEOS.Q()
